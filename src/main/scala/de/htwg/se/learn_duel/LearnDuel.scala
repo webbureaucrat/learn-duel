f275b807d53c2158772a677e4b29f7fae9e4ef14
Jonas Reinwald <jonas.reinwald@htwg-konstanz.de>
2017-11-08 11:27:42 +0100

    renamed main file

--- src/main/scala/de/htwg/se/learn_duel/LearnDuel.scala
+++ src/main/scala/de/htwg/se/learn_duel/LearnDuel.scala
@@ -2,7 +2,7 @@ package de.htwg.se.learn_duel
 
 import de.htwg.se.learn_duel.ui.impl.TUI
 
-object YourGame {
+object LearnDuel {
     def main(args: Array[String]): Unit = {
         println(TUI.createTUI.dummyText)
     }
