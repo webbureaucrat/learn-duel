7f3ca60ff5e53719b9339f1a1cdead6c46934340
Jonas Reinwald <jonas.reinwald@htwg-konstanz.de>
2018-01-24 21:46:30 +0100

    added model/answer test

--- src/test/scala/de/htwg/se/learn_duel/model/AnswerSpec.scala
+++ src/test/scala/de/htwg/se/learn_duel/model/AnswerSpec.scala
@@ -1,5 +1,33 @@
 package de.htwg.se.learn_duel.model
 
-class AnswerSpec {
+import de.htwg.se.learn_duel.model.impl.{Answer => AnswerImpl}
+import org.junit.runner.RunWith
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+import play.api.libs.json.Json
 
+@RunWith(classOf[JUnitRunner])
+class AnswerSpec extends WordSpec with Matchers {
+    "A Answer" when {
+        "new" should {
+            val answer = AnswerImpl(0, "text")
+            "a ID" in {
+                answer.id should be(0)
+            }
+            "have a text" in {
+                answer.text should be("text")
+            }
+        }
+    }
+
+    "A Answer" should {
+        "be serializable to JSON" in {
+            val answer = AnswerImpl(0, "text")
+
+            val jsonValue = Json.parse("{\"id\": 0, \"text\": \"text\"}")
+
+            Json.toJson(answer) should be(jsonValue)
+        }
+    }
 }
+
