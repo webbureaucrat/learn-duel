22d29b6b11c2e6c197366d34e6f28ddc1146c325
Jonas Reinwald <jonas.reinwald@htwg-konstanz.de>
2018-01-24 21:12:31 +0100

    added model/player test

--- src/test/scala/de/htwg/se/learn_duel/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/learn_duel/model/PlayerSpec.scala
@@ -1,6 +1,8 @@
 package de.htwg.se.learn_duel.model
 
-import de.htwg.se.learn_duel.model.impl.{ Player => PlayerImpl}
+import java.security.InvalidParameterException
+
+import de.htwg.se.learn_duel.model.impl.{Player => PlayerImpl}
 import org.junit.runner.RunWith
 import org.scalatest._
 import org.scalatest.junit.JUnitRunner
@@ -9,14 +11,37 @@ import org.scalatest.junit.JUnitRunner
 class PlayerSpec extends WordSpec with Matchers {
     "A Player" when {
         "new" should {
-            val player = PlayerImpl("Your Name")
+            val player = PlayerImpl("YourName")
             "have a name" in {
-                player.name should be("Your Name")
+                player.name should be("YourName")
             }
             "have a nice String representation" in {
-                player.toString should be("Your Name")
+                player.toString should be("YourName")
+            }
+            "have no points" in {
+                player.points should be(0)
+            }
+            "have no correct answers" in {
+                player.correctAnswers.length should be (0)
+            }
+            "have no wrong answers" in {
+                player.wrongAnswers.length should be (0)
             }
         }
+
+        "not accept emtpy names" in {
+            assertThrows[InvalidParameterException] {
+                val player = PlayerImpl("")
+            }
+        }
+
+        "not accept names with whitespace" in {
+            assertThrows[InvalidParameterException] {
+                val player = PlayerImpl("Your Name")
+            }
+        }
+
+
     }
 }
 
