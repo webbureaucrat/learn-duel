b67a1d578cd673627146eb1813a978163c2d38ff
Jonas Reinwald <jonas.reinwald@htwg-konstanz.de>
2018-01-24 21:47:26 +0100

    added model/question test

--- src/test/scala/de/htwg/se/learn_duel/model/QuestionSpec.scala
+++ src/test/scala/de/htwg/se/learn_duel/model/QuestionSpec.scala
@@ -1,5 +1,47 @@
 package de.htwg.se.learn_duel.model
 
-class QuestionSpec {
+import de.htwg.se.learn_duel.model.impl.{Answer => AnswerImpl, Question => QuestionImpl}
+import org.junit.runner.RunWith
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+import play.api.libs.json.Json
 
+@RunWith(classOf[JUnitRunner])
+class QuestionSpec extends WordSpec with Matchers {
+    "A Question" when {
+        "new" should {
+            val answers = List(AnswerImpl(0, "text"), AnswerImpl(1, "text2"))
+            val question = QuestionImpl(0, "text", 20, answers, 0, 20)
+            "a ID" in {
+                question.id should be(0)
+            }
+            "have a text" in {
+                question.text should be("text")
+            }
+            "have a point value" in {
+                question.points should be(20)
+            }
+            "have a list of answers" in {
+                question.answers should be (answers)
+            }
+            "a correct answer" in {
+                question.correctAnswer should be(0)
+            }
+            "a time" in {
+                question.time should be(20)
+            }
+        }
+    }
+
+    "A Question" should {
+        "be serializable to JSON" in {
+            val answers = List(AnswerImpl(0, "text"))
+            val question = QuestionImpl(0, "text", 20, answers, 0, 20)
+
+            val jsonValue = Json.parse("{\"id\": 0, \"text\": \"text\", \"points\": 20, \"answers\": [{ \"id\": 0, \"text\": \"text\" }],\"correctAnswer\": 0, \"time\": 20}")
+
+            Json.toJson(question) should be(jsonValue)
+        }
+    }
 }
+
