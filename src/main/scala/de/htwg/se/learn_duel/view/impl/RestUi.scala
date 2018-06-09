87f8ccadae4b90dcaaaa6b242a9821ce39031dd0
Julian Raufelder <Julian@Raufelder.com>
2018-06-09 10:26:07 +0200

    #10 enhance akka implementation

--- src/main/scala/de/htwg/se/learn_duel/view/impl/RestUi.scala
+++ src/main/scala/de/htwg/se/learn_duel/view/impl/RestUi.scala
@@ -2,9 +2,9 @@ package de.htwg.se.learn_duel.view.impl
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{Route, StandardRoute}
 import akka.stream.ActorMaterializer
 import de.htwg.se.learn_duel.controller.Controller
 
@@ -14,76 +14,61 @@ class RestUi(controller: Controller) {
 
   implicit val system: ActorSystem = ActorSystem("system")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
-  // needed for the future flatMap/onComplete in the end
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
   val route: Route =
     get {
-      pathSingleSlash {
-        // FIXME #10 redirect to game
-        complete(
-          HttpEntity(ContentTypes.`text/plain(UTF-8)`, "<h1>Learn Duel</h1>"))
-      }
-      pathPrefix("rest" / "v1") {
+      pathPrefix("rest") {
         pathEnd {
-          // FIXME #10 give an overview over all API endpoint (json)
-          /**
-            * GET         /                                           Controller.game
-            * GET         /rest/v1/help                               Controller.onHelp
-            * GET         /rest/v1/game                               Controller.onStartGame
-            * GET         /rest/v1/cleanState                         Controller.onReset
-            * PUT         /rest/v1/player/name                        Controller.onAddPlayer(name)
-            * DELETE      /rest/v1/player/name                        Controller.onRemovePlayer(name)
-            * POST        /rest/v1/answer/answer                      Controller.onAnswerChosen(answer:Int)
-            * GET         /rest/v1/maxPlayerCount                     Controller.getMaxPlayerCount
-            */
-
-          complete("")
+          usage()
         } ~
-          path("help") {
+          pathSingleSlash {
+            usage()
+          } ~
+          path("v1" / "help") {
             controller.onHelp()
-            complete("")
+            complete(StatusCodes.OK)
           } ~
-          path("game") {
+          path("v1" / "game") {
             controller.onStartGame()
-            complete("")
+            complete(StatusCodes.OK)
           } ~
-          path("cleanState") {
+          path("v1" / "cleanState") {
             controller.reset()
-            complete("")
+            complete(StatusCodes.OK)
           } ~
-          path("maxPlayerCount") {
+          path("v1" / "maxPlayerCount") {
             complete(controller.maxPlayerCount.toString)
           }
       }
     } ~
       post {
-        pathPrefix("rest" / "v1") {
-          path("answer" / IntNumber) { id => {
+        pathPrefix("rest") {
+          path("v1" / "answer" / IntNumber) { id => {
             controller.onAnswerChosen(id)
-            complete("")
+            complete(StatusCodes.OK)
           }
           }
         }
       } ~
       put {
-        pathPrefix("rest" / "v1") {
-          path("player") {
+        pathPrefix("rest") {
+          path("v1" / "player") {
             controller.onAddPlayer(None)
-            complete("")
+            complete(StatusCodes.OK)
           } ~
-            path("player" / Segment) { name => {
+            path("v1" / "player" / Segment) { name => {
               controller.onAddPlayer(Some(name))
-              complete("")
+              complete(StatusCodes.OK)
             }
             }
         }
       } ~
       delete {
-        pathPrefix("rest" / "v1") {
-          path("player" / Segment) { name => {
+        pathPrefix("rest") {
+          path("v1" / "player" / Segment) { name => {
             controller.onRemovePlayer(name)
-            complete("")
+            complete(StatusCodes.OK)
           }
           }
         }
@@ -91,6 +76,22 @@ class RestUi(controller: Controller) {
 
   val bindingFuture: Future[Http.ServerBinding] =
     Http().bindAndHandle(route, "localhost", 8080)
+
+  def usage(): StandardRoute = {
+    lazy val message =
+      """Learn Duel rest API
+      GET     /rest                    Display this usage
+      GET     /rest/v1/help            Controller.onHelp
+      GET     /rest/v1/game            Controller.onStartGame
+      GET     /rest/v1/cleanState      Controller.onReset
+      PUT     /rest/v1/player/name     Controller.onAddPlayer(name)
+      DELETE  /rest/v1/player/name     Controller.onRemovePlayer(name)
+      POST    /rest/v1/answer/answer   Controller.onAnswerChosen(answer:Int)
+      GET     /rest/v1/maxPlayerCount  Controller.getMaxPlayerCount"""
+
+    complete(
+      HttpEntity(ContentTypes.`text/plain(UTF-8)`, message))
+  }
 
   def unbind(): Unit = {
     bindingFuture
